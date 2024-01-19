a0e1dc5cca5740f8f7980c7baeed4aa9977790e0
priya2208VP <115474562+priya2208VP@users.noreply.github.com>
2024-01-19 22:43:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+
+// Your existing function to navigate to a URL
+function navigateTo(url) {
+    window.location.href = url;
+}
+
+// script.js
+
+// Function to open the Google Form link in a new tab
+function openGoogleForm() {
+    window.open("https://forms.gle/XYsBWnU6odKcXo6QA", "_blank");
+}
+
